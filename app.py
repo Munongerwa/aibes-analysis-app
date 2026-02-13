f0377ce0b0752fe5731648777997c3e6a4c411f9
Munongerwa <batiraishemunongerwa960@gmail.com>
2026-02-13 08:07:55 +0200

    depoly to render

--- app.py
+++ app.py
@@ -517,5 +517,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     
     # Run the app
-    # app.run(host='0.0.0.0', port=port, debug=False)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+  #  app.run(debug=True)
